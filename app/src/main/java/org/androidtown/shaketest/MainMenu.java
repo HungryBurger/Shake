5dec0bd6d1e3705b0bc6546398c1b8f091da1c5b
MCJoo <hjjooace@naver.com>
2018-05-09 00:16:04 +0900

    Menu access button

--- app/src/main/java/org/androidtown/shaketest/MainMenu.java
+++ app/src/main/java/org/androidtown/shaketest/MainMenu.java
@@ -1,7 +1,11 @@
 package org.androidtown.shaketest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),ItemListActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),MainMenu.class);
+                startActivity(intent);
+            }
+        });
+        Button button3 = (Button) findViewById(R.id.button3);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),MainMenu.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
