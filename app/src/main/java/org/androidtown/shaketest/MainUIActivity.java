596d719d477cb1d86347e6172ed32555b34eb575
Hee-su Lee <hesue615@naver.com>
2018-05-11 01:39:33 +0900

    Edit basic color to White Adjust log-in-pressed

---
+++ app/src/main/java/org/androidtown/shaketest/MainUIActivity.java
@@ -0,0 +1,15 @@
+package org.androidtown.shaketest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainUIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_ui);
+
+
+    }
+}
