2ba2f5c66076442e97321f973cefd924b8fc0980
hjhng125 <33080926+hjhng125@users.noreply.github.com>
2018-05-11 19:03:49 +0900

    hon

--- app/src/main/java/org/androidtown/shaketest/MainActivity.java
+++ app/src/main/java/org/androidtown/shaketest/MainActivity.java
@@ -1,24 +1,24 @@
 package org.androidtown.shaketest;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.telephony.PhoneNumberUtils;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -27,20 +27,17 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "GoogleActivity";
     private static final int RC_SIGN_IN = 9001;
-
     // [START declare_auth]
     private FirebaseAuth mAuth;
     // [END declare_auth]
-
     private GoogleSignInClient mGoogleSignInClient;
-    //private TextView mStatusTextView;
-    //private TextView mDetailTextView;
+    private String displayUserName;
+    private String displayUserEmail;
+    private String displayUserPhoneNumber;
 
 
     @Override
@@ -52,7 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.sign_in_button).setOnClickListener(this);
         findViewById(R.id.sign_out_button).setOnClickListener(this);
         findViewById(R.id.disconnect_button).setOnClickListener(this);
-
+        findViewById(R.id.readContact).setOnClickListener(this);
         // [START config_signin]
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -66,8 +63,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // [START initialize_auth]
         mAuth = FirebaseAuth.getInstance();
         // [END initialize_auth]
-
-
     }
 
     // [START on_start_check_user]
@@ -92,7 +87,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 // Google Sign In was successful, authenticate with Firebase
                 GoogleSignInAccount account = task.getResult(ApiException.class);
                 firebaseAuthWithGoogle(account);
-
                 startActivity(new Intent(getApplicationContext(), MainUIActivity.class));
             } catch (ApiException e) {
                 // Google Sign In failed, update UI appropriately
@@ -121,9 +115,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            int num=892;
-                            DatabaseReference mRef = FirebaseDatabase.getInstance().getReference().child("이희수병신새끼");
-                            mRef.push().setValue(num);
+
                             updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
@@ -175,20 +167,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 });
     }
 
+    private void myContact() {
+
+        TelephonyManager telephonyManager = (TelephonyManager) getApplicationContext().
+                getSystemService(Context.TELEPHONY_SERVICE);
+
+        try {
+            String phoneNum = telephonyManager.getLine1Number();
+            if(phoneNum.startsWith("+82")) {
+                phoneNum = phoneNum.replace("+82", "0");
+            }
+            displayUserPhoneNumber = PhoneNumberUtils.formatNumber(phoneNum);
+            Toast.makeText(getApplicationContext(),
+                    displayUserName + " " + displayUserPhoneNumber + " " + displayUserEmail,
+                    Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "myContact: num");
+        } catch (SecurityException e) {
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private void updateUI(FirebaseUser user) {
         hideProgressDialog();
+        displayUserName = user.getDisplayName();
+        displayUserEmail = user.getEmail();
         if (user != null) {
-            //mStatusTextView.setText(user.getEmail());
-            //mDetailTextView.setText(user.getUid());
-
             findViewById(R.id.sign_in_button).setVisibility(View.GONE);
             findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+            findViewById(R.id.readContact).setVisibility(View.VISIBLE);
         } else {
-            //mStatusTextView.setText("Signed Out");
-            //mDetailTextView.setText(null);
-
             findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
             findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+            findViewById(R.id.readContact).setVisibility(View.GONE);
         }
     }
 
@@ -201,6 +211,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             signOut();
         } else if (i == R.id.disconnect_button) {
             revokeAccess();
+        } else if (i == R.id.readContact) {
+            myContact();
         }
     }
 
