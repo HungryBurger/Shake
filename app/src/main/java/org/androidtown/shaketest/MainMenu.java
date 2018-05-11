0f4522cd01705e572ac4e2129d07a2190ea3dcb6
MCJoo <hjjooace@naver.com>
2018-05-12 07:01:20 +0900

    Merge Final

--- app/src/main/java/org/androidtown/shaketest/MainMenu.java
+++ app/src/main/java/org/androidtown/shaketest/MainMenu.java
@@ -1,14 +1,21 @@
 package org.androidtown.shaketest;
 
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
+import android.telephony.TelephonyManager;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,7 +25,16 @@ import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainMenu extends AppCompatActivity {
     private String[] navItems = {"Contact List", "Customized", "Load", "Logout"};
@@ -28,6 +44,12 @@ public class MainMenu extends AppCompatActivity {
     private DrawerLayout dlDrawer;
     private ActionBarDrawerToggle dtToggle;
     private boolean isopen = false;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private FirebaseAuth.AuthStateListener mListener;
+    private String displayUserName, displayUserEmail, displayUserPhoneNumber;
+    // [END declare_auth]
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +88,95 @@ public class MainMenu extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        mAuth = FirebaseAuth.getInstance();
+        mListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                mUser = mAuth.getCurrentUser();
+
+                if (mUser != null) {
+                    displayUserName = mUser.getDisplayName();
+                    displayUserEmail = mUser.getEmail();
+                    getPhonenum();
+                    callDialog();
+                } else {
+                    startActivity(new Intent(MainMenu.this, MainActivity.class));
+                    finish();
+                }
+            }
+        };
+
+
+        findViewById(R.id.sign_out_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainMenu.this);
+                builder.setMessage("로그아웃 하시겠습니까?")
+                        .setCancelable(false)
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                signOut();
+                            }
+                        }).setNegativeButton("취소", null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if(mListener != null) {
+            mAuth.removeAuthStateListener(mListener);
+        }
+    }
+
+    private void getPhonenum() {
+        TelephonyManager telephonyManager = (TelephonyManager) getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
+
+        try {
+            String phoneNum = telephonyManager.getLine1Number();
+            if (phoneNum.startsWith("+82")) {
+                phoneNum = phoneNum.replace("+82", "0");
+            }
+            displayUserPhoneNumber = PhoneNumberUtils.formatNumber(phoneNum);
+        } catch (SecurityException e) {
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void signOut() {
+        // Firebase sign out
+        mAuth.signOut();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        // Google sign out
+        mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                    }
+                });
+    }
+
+    private void callDialog() {
+        FragmentManager fm = getSupportFragmentManager();
+        MyAlertDialogFragment newDialogFragment = MyAlertDialogFragment.newInstance(displayUserName, displayUserPhoneNumber, displayUserEmail);
+        newDialogFragment.show(fm, "dialog");
     }
 
     protected void onPostCreate(Bundle savedInstanceState) {
