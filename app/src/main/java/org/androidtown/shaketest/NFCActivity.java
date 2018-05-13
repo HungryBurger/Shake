81cb0fa961fb57ce62cfc7d698abea5b7bae8908
hjhng125 <33080926+hjhng125@users.noreply.github.com>
2018-05-13 20:20:16 +0900

    NFC

--- app/src/main/java/org/androidtown/shaketest/NFCActivity.java
+++ app/src/main/java/org/androidtown/shaketest/NFCActivity.java
@@ -6,7 +6,6 @@ import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
