8b3732b27a1e034096ee4c9b40c8b4df61897f09
hjhng125 <33080926+hjhng125@users.noreply.github.com>
2018-05-13 18:05:50 +0900

    hjhng125

--- app/src/main/java/org/androidtown/shaketest/MainMenu.java
+++ app/src/main/java/org/androidtown/shaketest/MainMenu.java
@@ -1,21 +1,32 @@
 package org.androidtown.shaketest;
 
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.media.Image;
+import android.nfc.NfcAdapter;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,9 +36,12 @@ import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -36,14 +50,20 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainMenu extends AppCompatActivity {
-    private String[] navItems = {"Contact List", "Customized", "Load", "Logout"};
-    private ListView lvNavList;
-    private FrameLayout flContainer;
-    private ToggleButton service_btn = null;
-    private DrawerLayout dlDrawer;
-    private ActionBarDrawerToggle dtToggle;
-    private boolean isopen = false;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+
+public class MainMenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle drawerToggle;
+    Toolbar toolbar;
+    View nav_header_view;
+    TextView mName, mPhoneNum,mEmail;
+    ImageButton mPicture;
+    private static int GET_PICTURE_URI = 9999;
+    NfcAdapter nfcAdapter;
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
     private FirebaseAuth.AuthStateListener mListener;
@@ -56,38 +76,8 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        initLayout();
 
-        lvNavList = (ListView) findViewById(R.id.lv_activity_main_nav_list);
-        flContainer = (FrameLayout) findViewById(R.id.realcontents);
-
-        lvNavList.setAdapter(new ArrayAdapter<String>(MainMenu.this,
-                android.R.layout.simple_list_item_1, navItems));
-        lvNavList.setOnItemClickListener(new DrawerItemClickListener());
-
-        dlDrawer = (DrawerLayout) findViewById(R.id.dl_main_drawer);
-        View button1 = findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                if (dlDrawer.isDrawerOpen(Gravity.LEFT)) {
-                    dlDrawer.closeDrawer(Gravity.LEFT);
-                } else {
-                    dlDrawer.openDrawer(Gravity.LEFT);
-                }
-                isopen = !isopen;
-            }
-        });
-        ImageButton setting = (ImageButton) findViewById(R.id.button2);
-        setting.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainMenu.this, SettingsActivity.class);
-                startActivity(intent);
-            }
-        });
         mAuth = FirebaseAuth.getInstance();
         mListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -98,39 +88,166 @@ public class MainMenu extends AppCompatActivity {
                     displayUserName = mUser.getDisplayName();
                     displayUserEmail = mUser.getEmail();
                     getPhonenum();
-                    callDialog();
+                    //callDialog();
+                    setProfile();
                 } else {
                     startActivity(new Intent(MainMenu.this, MainActivity.class));
                     finish();
                 }
             }
         };
-
-
-        findViewById(R.id.sign_out_button).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.popup).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainMenu.this);
-                builder.setMessage("로그아웃 하시겠습니까?")
-                        .setCancelable(false)
-                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                signOut();
-                            }
-                        }).setNegativeButton("취소", null);
-                AlertDialog dialog = builder.create();
-                dialog.show();
+                callDialog();
             }
         });
+        onNFC();
     }
+    private void onNFC(){
+        nfcAdapter = NfcAdapter.getDefaultAdapter(MainMenu.this);
+        if(nfcAdapter.isEnabled()){}
+        else{
+            AlertDialog.Builder alertBox= new AlertDialog.Builder(MainMenu.this);
+            alertBox.setTitle("NFC Connection ERROR....");
+            alertBox.setMessage("NFC를 켜주세요.");
+            alertBox.setPositiveButton("Turn on", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    if(Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN)
+                        startActivity(new Intent(Settings.ACTION_NFC_SETTINGS));
+                    else
+                        startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+                }
+            });
+            alertBox.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            alertBox.show();
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == GET_PICTURE_URI){
+            if(resultCode == Activity.RESULT_OK){
+                try{
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),data.getData());
+                    mPicture.setImageBitmap(bitmap);
+                    Glide.with(MainMenu.this).load(data.getData()).diskCacheStrategy(DiskCacheStrategy.SOURCE).into(mPicture);
+                }catch (IOException e){
+                    Log.e("TAG",e.getMessage());
+                }
+            }
+        }
+    }
+
+    private void setProfile(){
+        navigationView.setNavigationItemSelectedListener(MainMenu.this);
+        nav_header_view = navigationView.getHeaderView(0);
+
+        mEmail = (TextView)nav_header_view.findViewById(R.id.profile_E_mail);
+        mEmail.setSelected(true);
+        mName = (TextView)nav_header_view.findViewById(R.id.profile_name);
+        mPhoneNum = (TextView)nav_header_view.findViewById(R.id.profile_phone_number);
+        mPicture = (ImageButton) nav_header_view.findViewById(R.id.profile_picture);
+        mPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,GET_PICTURE_URI);
+            }
+        });
+        mEmail.setText(displayUserEmail);
+        mName.setText(displayUserName);
+        mPhoneNum.setText(displayUserPhoneNumber);
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "item1 clicked..", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item2:
+                Toast.makeText(this, "item2 clicked..", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.log_out:
+                click_log_out();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return false;
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void initLayout(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher_round);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.dl_main_drawer_root);
+        navigationView = (NavigationView) findViewById(R.id.nv_main_navigation_root);
+        drawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.open_drawer,
+                R.string.close_drawer
+        );
+        drawerLayout.addDrawerListener(drawerToggle);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    public void click_log_out(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainMenu.this);
+        builder.setMessage("로그아웃 하시겠습니까?")
+                .setCancelable(false)
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        signOut();
+                    }
+                }).setNegativeButton("취소", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
-
         mAuth.addAuthStateListener(mListener);
     }
-
     @Override
     protected void onStop() {
         super.onStop();
@@ -139,7 +256,6 @@ public class MainMenu extends AppCompatActivity {
             mAuth.removeAuthStateListener(mListener);
         }
     }
-
     private void getPhonenum() {
         TelephonyManager telephonyManager = (TelephonyManager) getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
 
@@ -178,48 +294,4 @@ public class MainMenu extends AppCompatActivity {
         MyAlertDialogFragment newDialogFragment = MyAlertDialogFragment.newInstance(displayUserName, displayUserPhoneNumber, displayUserEmail);
         newDialogFragment.show(fm, "dialog");
     }
-
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
-    }
-
-    private class DrawerItemClickListener implements
-            ListView.OnItemClickListener {
-
-        @Override
-        public void onItemClick(AdapterView<?> adapter, View view,
-                                int position, long id) {
-            switch (position) {
-                case 0:
-                    break;
-                case 1:
-
-                    break;
-                case 2:
-
-                    break;
-                case 3:
-
-                    break;
-                case 4:
-                    break;
-
-            }
-            dlDrawer.closeDrawer(lvNavList);
-
-        }
-
-    }
-
 }
