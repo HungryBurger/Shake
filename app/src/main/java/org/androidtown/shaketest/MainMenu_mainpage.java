a574b809465ab0aa171afef63d45c61167271ae7
hjhng125 <33080926+hjhng125@users.noreply.github.com>
2018-05-23 00:16:34 +0900

    홍

--- app/src/main/java/org/androidtown/shaketest/MainMenu_mainpage.java
+++ app/src/main/java/org/androidtown/shaketest/MainMenu_mainpage.java
@@ -27,14 +27,7 @@ public class MainMenu_mainpage extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup mView = (ViewGroup) inflater.inflate(R.layout.activity_main_menu_mainpage, container, false);
-        CircleImageView imgBtn = mView.findViewById(R.id.profile_image);
-        imgBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                activity = (MainMenu)getActivity();
-                activity.imageDialog();
-            }
-        });
+
         return mView;
     }
 }
