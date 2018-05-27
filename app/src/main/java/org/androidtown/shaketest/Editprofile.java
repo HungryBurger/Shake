00a529499a530080d4bd8fb0f87dab8fa5e87403
MCJoo <hjjooace@naver.com>
2018-05-28 04:47:43 +0900

    Edit Menu

--- app/src/main/java/org/androidtown/shaketest/Editprofile.java
+++ app/src/main/java/org/androidtown/shaketest/Editprofile.java
@@ -1,17 +1,24 @@
 package org.androidtown.shaketest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.qrcode.encoder.QRCode;
+import com.journeyapps.barcodescanner.ViewfinderView;
 
 import java.util.ArrayList;
 
 public class Editprofile extends Fragment {
-    ArrayList<CustomizedAdapter.Item> list;
-
+    private FragmentManager fragmentManager;
     public static Editprofile newInstance() {
         Bundle args = new Bundle();
         Editprofile fragment = new Editprofile();
@@ -24,6 +31,25 @@ public class Editprofile extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         ViewGroup mView = (ViewGroup) inflater.inflate(R.layout.activity_editprofile, container, false);
+        fragmentManager = getActivity().getSupportFragmentManager();
+        Button a = (Button)mView.findViewById(R.id.button1);
+        Button b = (Button)mView.findViewById(R.id.button2);
+
+        a.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getActivity(),CustomScannerActivity.class);
+                Toast.makeText(getActivity(), "QR코드가 눌렸습니다.", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
+        b.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                fragmentManager.beginTransaction().replace(R.id.frameLayout, Customized_main.newInstance()).commit();
+                Toast.makeText(getActivity(), "커스터마이징 수정하기가 눌렸습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
         return mView;
     }
 }
